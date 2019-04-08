975e330247ccf03b1295de8a94ef711f9aeb9cc6
Matt <mhoang1@Matt-Aero15.localdomain>
2019-04-07 23:40:13 -0500

    using nextjs

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Header from '../comps/header';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <p>About page</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import Header from '../comps/header';
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+      {/* <Link href='/about'>
+        <button>about</button>
+      </Link> */}
+      <p>Hello</p>
+    </div>
+  );
+}
+  
+export default Index;
